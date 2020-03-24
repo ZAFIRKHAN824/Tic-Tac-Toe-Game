dfdd2e9d73aa442e9279d4446a8b2c063dac07a2
ZAFIRKHAN824 <zafirkhan824@gmail.com>
2020-03-24 20:04:04 +0500

    completed

--- config.cpp
+++ config.cpp
@@ -7,7 +7,8 @@ _location WINDOW_SIZE = {80, 41};
 _location START_POS = {WINDOW_SIZE.x/2, WINDOW_SIZE.y/2};
 _location BOARD_LINE_THICKNESS = {2,1};
 _location LINE_GAP = {5,3};
-const int BOARD_SIZE = 3;
+int BOARD_SIZE = 3;
+string BORD_BG_COLOR="Yellow";
 
 char PLAYER_1_MARKER='X';
 char PLAYER_2_MARKER='O';
--- main.cpp
+++ main.cpp
@@ -4,25 +4,20 @@
 #include<stdlib.h>
 using namespace std;
 
+
+//declaring signatures
+void branch_menu();
+void start_game();
+void main_menu();
+
 struct State {
-    char main_array[BOARD_SIZE][BOARD_SIZE];
+    vector<vector<char>> main_array;
     bool curr_player;
     _location curr_pos;
     bool game_over;
 };
+
 State state;
-
-//fill initial state array
-void fill_array(char arr[][BOARD_SIZE], char marker){
-    for(int i=0; i<BOARD_SIZE; i++)
-        {
-            for(int j=0; j<BOARD_SIZE; j++)
-            {
-                arr[i][j]=marker;
-            }
-
-        }
-}
 
 void init(){
     setConsoleDimensions(WINDOW_SIZE.x, WINDOW_SIZE.y);
@@ -33,8 +28,7 @@ void init(){
 }
 
 
-char GetArrowKeysInput()
-{
+char GetArrowKeysInput(){
     while(1)
     {
     switch(getch())
@@ -49,11 +43,25 @@ char GetArrowKeysInput()
         return 'R';
     case 13:
         return 'E';
+    case 27:
+        return 'B';
     }
     }
 }
 
+void userInput(){
+    cout << "\n\n     PRESS ENTER TO PLAY AGAIN AND PRESS ESCAPE TO GET TO MENU";
+            char Input=GetArrowKeysInput();
+            if(Input=='E')
+                start_game();
+            else if(Input=='B')
+                main_menu();
+}
+
 void renderBoard(){
+
+
+
     for(int i=0; i<BOARD_SIZE+1 ; i++)
     {
         //horizontal line
@@ -69,7 +77,9 @@ void renderBoard(){
         int lvx1 = START_POS.x - LINE_GAP.x/2 - (BOARD_SIZE/2 - i)*LINE_GAP.x - (BOARD_SIZE/2 + 1 - i)*BOARD_LINE_THICKNESS.x;
         int lvx2 = lvx1 + BOARD_LINE_THICKNESS.x;
         clrdLine(BOARD_CLR, lvx1, lvy1, lvx2, lvy2);
-    }
+
+
+}
 }
 
 void printMarkerOnBoard(char marker, int x, int y){
@@ -81,8 +91,7 @@ void printMarkerOnBoard(char marker, int x, int y){
     cout << marker;
 }
 
-void stateArray2Board()
-{
+void stateArray2Board(){
     for(int r=0; r<BOARD_SIZE; ++r){
         for(int c=0; c<BOARD_SIZE; ++c){
             printMarkerOnBoard(state.main_array[r][c], c, r);
@@ -91,14 +100,11 @@ void stateArray2Board()
 }
 
 char getCurrMarker(bool currPlayer){
-    if(state.curr_player==0)
+    if(currPlayer==0)
     {
         return PLAYER_1_MARKER;
     }
-    else
-    {
-        return PLAYER_2_MARKER;
-    }
+    else return PLAYER_2_MARKER;
 }
 
 void gameLogic(int row=BOARD_SIZE, int col=BOARD_SIZE){
@@ -117,10 +123,19 @@ void gameLogic(int row=BOARD_SIZE, int col=BOARD_SIZE){
             else hCounter++;
         }
         if(hCounter == col){
-            cout << "Row " << r << r << r << " filled!";
-            state.game_over = true;
-            return;
-        }
+            if(BOARD_SIZE==9)
+            {
+            Locate(5,40);
+            cout << "PLAYER WITH MARKER " << firstMarker << " WINS !";
+            userInput();
+            }
+            else
+            {
+            Locate(5,35);
+            cout << "PLAYER WITH MARKER " << firstMarker << " WINS !" ;
+            userInput();
+            }
+    }
     }
 
     //for cols
@@ -136,14 +151,23 @@ void gameLogic(int row=BOARD_SIZE, int col=BOARD_SIZE){
             else vCounter++;
         }
         if(vCounter == row){
-            cout << "Column " << c << " filled!";
-            state.game_over = true;
-            return;
-        }
+            if(BOARD_SIZE==9)
+            {
+            Locate(5,40);
+            cout << "PLAYER WITH MARKER " << firstMarker << " WINS !";
+            userInput();
+            }
+            else
+            {
+            Locate(5,35);
+            cout << "PLAYER WITH MARKER " << firstMarker << " WINS !" ;
+            userInput();
+            }
+
+
+
+    firstMarker = state.main_array[1][1];
     }
-
-     char firstMarker = state.main_array[1][1];
-
      //for left diagonal
      int ldCounter = 0;
      for(int r=0; r<row; ++r){
@@ -155,9 +179,18 @@ void gameLogic(int row=BOARD_SIZE, int col=BOARD_SIZE){
             else ldCounter++;
      }
      if(ldCounter == row){
-            cout << "Diagonal left filled!";
-            state.game_over = true;
-            return;
+            if(BOARD_SIZE==9)
+            {
+            Locate(5,40);
+            cout << "PLAYER WITH MARKER " << firstMarker << " WINS !";
+            userInput();
+            }
+            else
+            {
+            Locate(5,35);
+            cout << "PLAYER WITH MARKER " << firstMarker << " WINS !" ;
+            userInput();
+            }
     }
 
     //for right diagonal
@@ -171,24 +204,53 @@ void gameLogic(int row=BOARD_SIZE, int col=BOARD_SIZE){
             else rdCounter++;
      }
      if(rdCounter == row){
-            cout << "Diagonal right filled!";
-            state.game_over = true;
-            return;
+            if(BOARD_SIZE==9)
+            {
+            Locate(5,40);
+            cout << "PLAYER WITH MARKER " << firstMarker << " WINS !";
+            userInput();
+            }
+            else
+            {
+            Locate(5,35);
+            cout << "PLAYER WITH MARKER " << firstMarker << " WINS !" ;
+            userInput();
+            }
         }
 
     //check draw condition
     if(isDrawn){
-        cout << "Match Drawn";
-        WriteTextAtLoc("Match Drawn", 0, 0);
-        state.game_over = true;
-        return;
+        if(BOARD_SIZE==9)
+            {
+            Locate(5,40);
+            cout << "MATCH IS DRAWN";
+            userInput();
+            }
+            else
+            {
+            Locate(5,35);
+            cout << "MATCH IS DRAWN";
+            userInput();
+            }
     }
 
     state.curr_player= !state.curr_player;
 }
+}
 
-void getUserMove()
-{
+void show_current_player(){
+
+    if(state.curr_player==0){
+          Locate(2,3);
+          cout<<"PLAYER 1";
+          }
+          else{
+              Locate(2,3);
+          cout<<"PLAYER 2";
+          }
+}
+
+void getUserMove(){
     char Input=GetArrowKeysInput();
     if(Input=='U' && state.curr_pos.y > 0)
         state.curr_pos.y--;
@@ -199,18 +261,20 @@ void getUserMove()
     else if(Input=='R' && state.curr_pos.x < BOARD_SIZE-1)
         state.curr_pos.x++;
     else if(Input=='E') {
+
         if(state.main_array[state.curr_pos.y][state.curr_pos.x] == EMPTY_MARKER){
-            state.main_array[state.curr_pos.y][state.curr_pos.x]= getCurrMarker(state.curr_player);
+            state.main_array[state.curr_pos.y][state.curr_pos.x] = getCurrMarker(state.curr_player);
              gameLogic();
         }
     }
 }
 
-start_game(){
+void start_game(){
     system("cls");
 
     //set initial state
-   fill_array(state.main_array, EMPTY_MARKER);
+   //fill_array(state.main_array, EMPTY_MARKER);
+   state.main_array = vector<vector<char>>(BOARD_SIZE, vector<char>(BOARD_SIZE,EMPTY_MARKER));
     state.curr_player = 0;
     state.curr_pos = {0, 0};
     state.game_over = false;
@@ -218,19 +282,131 @@ start_game(){
     //render board
     renderBoard();
 
+
     //play game
     while(state.game_over == false)
     {
         stateArray2Board();
         printMarkerOnBoard(getCurrMarker(state.curr_player), state.curr_pos.x, state.curr_pos.y);
         getUserMove();
+        show_current_player();
     }
 
     system("cls");
 }
 
+void board_size(){
+    system("cls");
+    Menu boardSize({"3x3","5x5","7x7","9x9","BACK"});
+    boardSize.setHeading("SELECT BOARD SIZE");
+    int selectedSize = boardSize.drawMenu();
+    switch(selectedSize){
+        case 1:
+            BOARD_SIZE=3;
+            break;
+        case 2:
+            BOARD_SIZE=5;
+            break;
+        case 3:
+            BOARD_SIZE=7;
+            break;
+        case 4:
+            BOARD_SIZE=9;
+            break;
+        case 5:
+            break;
+            }
+    }
+
+void chnge_bg_color(){
+
+    system("cls");
+    Menu bg_color({"Red","Green","Yellow"});
+    switch(bg_color.drawMenu()){
+    case 1:
+        BG_CLR = "Red";
+        setBgClr(BG_CLR,1);
+        break;
+    case 2:
+        BG_CLR = "Green";
+        setBgClr(BG_CLR,1);
+        break;
+    case 3:
+        BG_CLR = "Yellow";
+        setBgClr(BG_CLR,1);
+        break;
+    }
+}
+
+void chnge_bord_color(){
+
+    system("cls");
+    Menu brd_color({"Black","Red","L Blue"});
+    switch(brd_color.drawMenu()){
+    case 1:
+        BOARD_CLR="Black";
+        break;
+    case 2:
+        BOARD_CLR="Red";
+        break;
+    case 3:
+        BOARD_CLR="Blue";
+        break;
+
+}
+}
+
+void branch_colour(){
+
+    system("cls");
+    Menu subBranch({"Back Ground Colour","Border Colour","Board Back Ground Colour"});
+    switch(subBranch.drawMenu()){
+    case 1:
+        chnge_bg_color();
+        break;
+    case 2:
+        chnge_bord_color();
+        break;
+    case 3:
+        break;
+
+}
+}
+
+void Custom_marker(){
+
+    system("cls");
+    char mark1,mark2;
+    cout<<"Select Player 1 Marker  ";
+    cin>>mark1;
+    PLAYER_1_MARKER=mark1;
+    cout<<"Select Player 2 Marker ";
+    cin>>mark2;
+    PLAYER_2_MARKER=mark2;
+}
+
+void branch_menu(){
+
+    system("cls");
+    Menu branchMenu({"Board size","Colour","Player mark"});
+    switch(branchMenu.drawMenu()){
+    case 1:
+        board_size();
+        break;
+    case 2:
+        branch_colour();
+        break;
+    case 3:
+        Custom_marker();
+        break;
+
+    }
+}
+
 void main_menu(){
+
     Menu _mainMenu({"2 players","Cpu vs player","setting","Exit"});
+    system("cls");
     switch(_mainMenu.drawMenu()){
     case 1:
         start_game();
@@ -238,6 +414,8 @@ void main_menu(){
     case 2:
         break;
     case 3:
+        branch_menu();
+        main_menu();
         break;
     case 4:
         break;
@@ -245,7 +423,11 @@ void main_menu(){
 }
 
 
-int main(){
+
+int main()
+{
     init();
     main_menu();
+    Locate(50,50);
+    return 0;
 }
